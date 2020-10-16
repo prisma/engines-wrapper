7126fe1756b220bc349e10b7a2bc62378f4090bd
timsuchanek <tim.suchanek@gmail.com>
2020-10-16 19:46:47 +0200

    chore: update gh action

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -3,6 +3,6 @@
 echo $NPM_TOKEN > ~/.npmrc
 
 set -ex
-npm i --silent -g pnpm@5.1.7 --unsafe-perm --ignore-scripts
-pnpm i
+npm i --silent -g pnpm@5.1.7 --unsafe-perm
+pnpm i --ignore-scripts --silent
 
