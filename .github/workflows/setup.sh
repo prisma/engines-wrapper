e8f70762a3ea9c2d9131314ada8399aab0e0214f
timsuchanek <tim.suchanek@gmail.com>
2020-10-19 11:32:07 +0200

    wip

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -4,6 +4,6 @@ echo $NPM_TOKEN > ~/.npmrc
 
 set -ex
 npm i --silent -g pnpm@5.1.7 esbuild@0.7.17 --unsafe-perm
-pnpm i --ignore-scripts --silent
+pnpm i --ignore-scripts
 pnpm recursive run build
 
