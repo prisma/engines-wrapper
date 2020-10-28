39110bf3ceab6b42ec7a23c4ec44804b37f69582
timsuchanek <tim.suchanek@gmail.com>
2020-10-28 15:42:20 +0100

    fix: bundle fetch-engine

--- packages/engines/scripts/build.js
+++ packages/engines/scripts/build.js
@@ -4,10 +4,15 @@ const chalk = require('chalk')
 async function main() {
   const before = Date.now()
 
+  // TODO: Combine download.ts and index.ts together to save space
   await Promise.all([
     run('tsc -d', true),
     run(
       'esbuild src/download.ts --outfile=download/index.js --bundle --platform=node --target=node10 --minify --sourcemap',
+      false,
+    ),
+    run(
+      'esbuild src/index.ts --outfile=dist/index.js --bundle --platform=node --target=node10 --minify --sourcemap',
       false,
     ),
   ])
