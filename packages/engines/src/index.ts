f3a43094f6bacf580e2c20a275ee87a41fb7b43a
timsuchanek <tim.suchanek@gmail.com>
2020-10-28 11:57:59 +0100

    fix: add engine path annotations

--- packages/engines/src/index.ts
+++ packages/engines/src/index.ts
@@ -5,3 +5,29 @@ export function getEnginesPath() {
 }
 
 export { enginesVersion } from '@prisma/engines-version'
+
+/**
+ * This annotation is used for `node-file-trace`
+ * See https://github.com/zeit/node-file-trace/issues/104
+ * It's necessary to run this package standalone or within the sdk in Vercel
+ */
+
+path.join(__dirname, '../query-engine-darwin')
+path.join(__dirname, '../introspection-engine-darwin')
+path.join(__dirname, '../prisma-fmt-darwin')
+
+path.join(__dirname, '../query-engine-debian-openssl-1.0.x')
+path.join(__dirname, '../introspection-engine-debian-openssl-1.0.x')
+path.join(__dirname, '../prisma-fmt-debian-openssl-1.0.x')
+
+path.join(__dirname, '../query-engine-debian-openssl-1.1.x')
+path.join(__dirname, '../introspection-engine-debian-openssl-1.1.x')
+path.join(__dirname, '../prisma-fmt-debian-openssl-1.1.x')
+
+path.join(__dirname, '../query-engine-rhel-openssl-1.0.x')
+path.join(__dirname, '../introspection-engine-rhel-openssl-1.0.x')
+path.join(__dirname, '../prisma-fmt-rhel-openssl-1.0.x')
+
+path.join(__dirname, '../query-engine-rhel-openssl-1.1.x')
+path.join(__dirname, '../introspection-engine-rhel-openssl-1.1.x')
+path.join(__dirname, '../prisma-fmt-rhel-openssl-1.1.x')
