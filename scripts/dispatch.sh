8e79580abcdd7911416287094600bf270117da66
timsuchanek <tim.suchanek@gmail.com>
2021-02-18 17:24:14 +0100

    chore: update lockfile

--- scripts/dispatch.sh
+++ scripts/dispatch.sh
@@ -2,5 +2,5 @@
 
 curl -H "Authorization: token $GH_TOKEN" \
 --request POST \
---data '{"event_type": "publish-engines", "client_payload": {"branch": "query-engine/second_coming_of_reserved_words", "commit": "5f311a3c26309b4d6cd89438cb1527011ba02057"}}' \
+--data '{"event_type": "publish-engines", "client_payload": {"branch": "integration/new-engine", "commit": "43e73b6d61a0d707a4b605daa6169624acc33b74"}}' \
 https://api.github.com/repos/prisma/engines-wrapper/dispatches
