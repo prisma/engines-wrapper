2f830f94a75611a0b6815c4c00461d08b7a1ac8b
timsuchanek <tim.suchanek@gmail.com>
2021-02-26 16:07:22 +0100

    chore: trigger master (dev) build

--- scripts/dispatch.sh
+++ scripts/dispatch.sh
@@ -2,5 +2,5 @@
 
 curl -H "Authorization: token $GH_TOKEN" \
 --request POST \
---data '{"event_type": "publish-engines", "client_payload": {"branch": "integration/new-engine", "commit": "43e73b6d61a0d707a4b605daa6169624acc33b74"}}' \
+--data '{"event_type": "publish-engines", "client_payload": {"branch": "master", "commit": "3138a37812de53df8f590343cb9b9fbbe2e33634"}}' \
 https://api.github.com/repos/prisma/engines-wrapper/dispatches
