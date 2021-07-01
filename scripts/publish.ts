536495fcda1d965d9ae2ba7b416b1b02aa8dcac1
Harshit Pant <pantharshit00@gmail.com>
2021-07-01 18:51:06 +0530

    chore: add comment for workflow command syntax

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -69,6 +69,8 @@ async function main(dryRun = false) {
   // Print out version for workflow dispatch if npmTag is latest
   if (npmTag === 'latest') {
     console.log(`Printing version for workflow dispatch: ${npmTag}`)
+    // This special log makes new_prisma_version avaliable in github actions
+    // Read: https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#using-workflow-commands-to-access-toolkit-functions
     console.log(`::set-output name=new_prisma_version::${npmTag}`)
   }
 }
