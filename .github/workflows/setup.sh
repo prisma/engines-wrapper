eced90fff1d8fae315cd6cfc131d48070a12bdfa
Joël <joel.galeran@gmail.com>
2021-07-07 11:27:22 +0200

    remove global esbuild and --ignore-scripts

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -4,8 +4,8 @@ echo $NPM_TOKEN > ~/.npmrc
 
 set -ex
 
-npm i --silent -g pnpm@6.9.1 esbuild@0.8.53 --unsafe-perm
+npm i --silent -g pnpm@6.9.1 --unsafe-perm
 
-pnpm i --no-prefer-frozen-lockfile --ignore-scripts
+pnpm i --no-prefer-frozen-lockfile
 
 pnpm recursive run build
