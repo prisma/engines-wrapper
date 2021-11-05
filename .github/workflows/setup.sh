62868f28b9993bb5fe320cd4b11afbadd337e06a
Joël Galeran <Jolg42@users.noreply.github.com>
2021-11-05 08:54:05 +0100

    ci: use latest pnpm@6 (#241)

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -5,7 +5,7 @@ echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
 
 set -ex
 
-npm i --silent -g pnpm@6.9.1 esbuild@0.8.53 --unsafe-perm
+npm i --silent -g pnpm@6 esbuild@0.8.53 --unsafe-perm
 
 pnpm i --no-prefer-frozen-lockfile --ignore-scripts
 
