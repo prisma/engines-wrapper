5b4a9cd91032a047b04a6d7c8b9c5fd5eb6ac06d
pierre <pierreantoine.urvoy@gmail.com>
2022-07-01 15:23:45 +0200

    chore: remove moved packages (engines/fetch-engine/get-platform) (#410)
    Co-authored-by: Joël Galeran <Jolg42@users.noreply.github.com>

--- packages/get-platform/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { getNodeAPIName } from './getNodeAPIName'
-export { getos, getPlatform } from './getPlatform'
-export { isNodeAPISupported } from './isNodeAPISupported'
-export { Platform, platforms } from './platforms'
